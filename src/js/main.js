412c31ee33fb25db994746145cf354a0dbb71f8c
despairpanta <xenia.mikhaleva@studentfr.ch>
2025-04-08 12:01:44 +0200

    ajout du carousel, animation footer,

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// cursor blend effect
 const cursor = document.querySelector(".cursor");
 document.addEventListener("mousemove", (e) => {
   cursor.style.left = `${e.clientX}px`;
@@ -11,4 +12,23 @@ document.querySelectorAll("a").forEach((link) => {
   link.addEventListener("mouseleave", () => {
     cursor.style.mixBlendMode = "color-dodge";
   });
+});
+
+// eye tracker
+const eyePupil = document.querySelector(".pupil");
+const innerEye = document.querySelector(".inner-eye");
+
+// carousel
+import Swiper, { Navigation, Pagination } from "swiper";
+
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
 });
